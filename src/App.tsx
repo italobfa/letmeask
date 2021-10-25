1448b2144034f3e02a439827c8c1015fa0f03e38
italobfa <italobfa@hotmail.com>
2021-10-25 20:02:55 -0300

    captura chave da sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState, createContext, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from "./services/firebase";
 import { AuthContextProvider } from "./contexts/AuthContext"
+import {Room} from "./pages/Room"
 
 
 function App() {
@@ -11,8 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -3,13 +3,14 @@ import logoImg from "../assets/images/logo.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { FormEvent, useState } from "react";
 import { database } from "../services/firebase";
 import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
-  const { user } = useAuth() 
+  const { user } = useAuth()
+  const history = useHistory() 
   const [newRoom, setNewRoom] = useState('');
 
   async function handleCreateRoom(event: FormEvent) {
@@ -25,6 +26,8 @@ export function NewRoom() {
       tittle: newRoom,
       authorId: user?.id,
     })
+
+    history.push(`/rooms/${firebaseRoom.key}`)
   }
 
   return (
