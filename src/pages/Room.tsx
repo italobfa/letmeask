d718a38a27230cd3374d81f27a081d2fa84cb215
italobfa <italobfa@hotmail.com>
2021-10-25 21:57:41 -0300

    copia codigo da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,22 @@
+import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
 import "../styles/room.scss";
 
+type ParamsProps = {
+  id: string;
+};
+
 export function Room() {
+  const params = useParams<ParamsProps>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
-          <img src={logoImg} alt="" />
-          <div>codigo</div>
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={params.id}/>
         </div>
       </header>
 
