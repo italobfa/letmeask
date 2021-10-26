0a532b3ac4d5dd38a818c642e1bb9dea410f5b43
italobfa <italobfa@hotmail.com>
2021-10-25 21:22:54 -0300

    page sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,33 @@
-export function Room(){
-  return(
-    <h1>Sala</h1>
-  )
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import "../styles/room.scss";
+
+export function Room() {
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="" />
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="Digite sua pergunta" />
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>
+            </span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
