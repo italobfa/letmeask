0556e5c5f2049f58d728d575338b4872828c423b
italobfa <italobfa@hotmail.com>
2021-10-26 22:12:04 -0300

    adiciona estrutura das perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
+import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
@@ -11,7 +12,7 @@ type ParamsProps = {
   id: string;
 };
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -40,8 +41,8 @@ export function Room() {
   const params = useParams<ParamsProps>();
   const roomId = params.id;
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
-  const [title, setTitle] = useState('')
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     const roomRef = database.ref(`rooms/${roomId}`);
@@ -63,7 +64,7 @@ export function Room() {
         }
       );
       setTitle(databaseRoom.title);
-      setQuestions(parsedQuestions)
+      setQuestions(parsedQuestions);
     });
   }, [roomId]);
 
@@ -106,7 +107,7 @@ export function Room() {
       <main>
         <div className="room-title">
           <h1>Sala {title}</h1>
-          { questions.length > 0 &&  <span>{questions.length} pergunta(s)</span>}
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
         <form onSubmit={handleSendQuestion}>
@@ -131,8 +132,20 @@ export function Room() {
             </Button>
           </div>
         </form>
-
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {
+            //Percorrendo o array e retornando no componente
+            questions.map((question) => {
+              return (
+                <Question
+                  key={question.id}
+                  content={question.content}
+                  author={question.author}
+                />
+              );
+            })
+          }
+        </div>
       </main>
     </div>
   );
